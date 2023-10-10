7e3eaffee1e359d35ce9c2443e3a26632ccb02a2
Aldo Santos <aldoacesso@gmail.com>
2023-10-10 14:21:40 -0300

    Criado componentes a nivel de atomo

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  @Input()
+  gameCover: string = ""
+
+  //card-label.component.ts
+  @Input()
+  gameLabel: string = ""
+
+  //card-pricing.component.ts
+  @Input()
+  gameType: string = ""
+  @Input()
+  gamePrice: string = ""
   constructor() { }
 
   ngOnInit(): void {
